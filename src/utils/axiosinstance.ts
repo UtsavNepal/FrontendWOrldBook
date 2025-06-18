0e1f8a837ec51abe18319cb43ce89f7704a35b51
Utsav Nepal <utsavnepal021@gmail.com>
2025-06-18 13:27:25 +0545

    changes

--- src/utils/axiosinstance.ts
+++ src/utils/axiosinstance.ts
@@ -1,11 +1,14 @@
 import axios, { AxiosResponse, AxiosError, InternalAxiosRequestConfig } from "axios";
 import { clearTokens, getAccessToken, refreshAccessToken } from "./tokenUtils";
 
-
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL, 
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,  // Enable sending cookies for CSRF
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  }
 });
-
 
 axiosInstance.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
@@ -21,20 +24,29 @@ axiosInstance.interceptors.request.use(
   }
 );
 
-
 axiosInstance.interceptors.response.use(
   (response: AxiosResponse) => response,
   async (error: AxiosError) => {
     const originalRequest = error.config as InternalAxiosRequestConfig & { _retry?: boolean };
 
+    // Log detailed error information
+    console.error('API Error:', {
+      status: error.response?.status,
+      data: error.response?.data,
+      headers: error.response?.headers,
+      config: {
+        url: error.config?.url,
+        method: error.config?.method,
+        headers: error.config?.headers,
+        withCredentials: error.config?.withCredentials
+      }
+    });
 
     if (error.response?.status === 401 && !originalRequest?._retry) {
       originalRequest._retry = true;
 
-  
       const newAccessToken = await refreshAccessToken();
       if (newAccessToken) {
-        
         originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
         return axiosInstance(originalRequest);
       }
